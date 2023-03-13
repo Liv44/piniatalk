ab1b7e0f9040e05e794118cd1c99712108563f47
Louis <louis.teilliais@gmail.com>
2023-03-13 14:35:38 +0100

    request channels

--- src/CRUD/channel.ts
+++ src/CRUD/channel.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const baseURL = "https://edu.tardigrade.land/msg";
 
+/* Create channel */
 export const createChannel = async (name: string, img: string) => {
     const response = await axios.put(`${baseURL}/protected/channel`, {
         name,
@@ -9,6 +10,68 @@ export const createChannel = async (name: string, img: string) => {
     }, {
         headers: {
             "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    })
+    return response.data;
+}
+
+/* Delete a channel */
+export const deleteChannel = async(channel_id: number) => {
+    const response = await axios.delete(`${baseURL}/protected/channel/${channel_id}`, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    })
+    return response.data;
+}
+
+/* Add user to channel */
+export const addUserToChannel = async(channel_id: number, user_id: string) => {
+    const response = await axios.put(`${baseURL}/protected/channel/${channel_id}/user/${user_id}`, {
+        user_id,
+        channel_id,
+    }, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    })
+    return response.data;
+}
+
+/* Ban a user */
+export const banUser = async(channel_id: number, user_id: string) => {
+    const response = await axios.delete(`${baseURL}/protected/channel/${channel_id}/user/${user_id}`,  {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    },)
+    return response.data;
+}
+
+/* Update name or image to a channel */
+export const updateChannel = async(channel_id: number, name: string, img: string) => {
+    const response = await axios.post(`${baseURL}/protected/channel/${channel_id}/update_metadata`, {
+        name,
+        img,
+    }, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    })
+    return response.data;
+}
+
+/* Get channels */
+export const getChannels = async() => {
+    const response = await axios.get(`${baseURL}/protected/user/channels`,{
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
     })
     return response.data;
