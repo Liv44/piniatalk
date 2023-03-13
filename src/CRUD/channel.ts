5f31f1f721fc1b4a897827d61c59646553146be0
Elouan <73824665+ByMSRT@users.noreply.github.com>
2023-03-13 15:15:00 +0100

    Merge

--- src/CRUD/channel.ts
+++ src/CRUD/channel.ts
@@ -27,3 +27,82 @@ export const createChannel = async (
     );
     return response.data;
 };
+
+/* Delete a channel */
+export const deleteChannel = async (channel_id: number) => {
+    const response = await axios.delete(
+        `${baseURL}/protected/channel/${channel_id}`,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        }
+    );
+    return response.data;
+};
+
+/* Add user to channel */
+export const addUserToChannel = async (channel_id: number, user_id: string) => {
+    const response = await axios.put(
+        `${baseURL}/protected/channel/${channel_id}/user/${user_id}`,
+        {
+            user_id,
+            channel_id,
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        }
+    );
+    return response.data;
+};
+
+/* Ban a user */
+export const banUser = async (channel_id: number, user_id: string) => {
+    const response = await axios.delete(
+        `${baseURL}/protected/channel/${channel_id}/user/${user_id}`,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        }
+    );
+    return response.data;
+};
+
+/* Update name or image to a channel */
+export const updateChannel = async (
+    channel_id: number,
+    name: string,
+    img: string
+) => {
+    const response = await axios.post(
+        `${baseURL}/protected/channel/${channel_id}/update_metadata`,
+        {
+            name,
+            img,
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        }
+    );
+    return response.data;
+};
+
+/* Get channels */
+export const getChannels = async () => {
+    const response = await axios.get(`${baseURL}/protected/user/channels`, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    });
+    return response.data;
+};
