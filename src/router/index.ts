d50e077067626ab8bdc9d4bc897fa17b1173686c
Elouan <73824665+ByMSRT@users.noreply.github.com>
2023-03-23 20:26:44 +0100

    Add webSockets system

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,33 +2,39 @@ import { createRouter, createWebHistory } from "vue-router";
 import ChannelVue from "../components/Channel.vue";
 import NotFound from "../views/NotFoundView.vue";
 import LoginVue from "../components/Login.vue";
+import WebSockets from "../views/WebSockets.vue";
 
 const isAuthenticated = async () => {
-  const isAuthenticated = sessionStorage.getItem('token')
-    if (!isAuthenticated && window.location.pathname !== '/login' ){
-        window.location.href = '/login'
+    const isAuthenticated = sessionStorage.getItem("token");
+    if (!isAuthenticated && window.location.pathname !== "/login") {
+        window.location.href = "/login";
     }
-}
+};
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/login",
-            name : "login",
-            component: LoginVue
+            name: "login",
+            component: LoginVue,
         },
         {
             path: "/channel/:id?",
             name: "channel",
             component: ChannelVue,
-            beforeEnter: isAuthenticated
+            beforeEnter: isAuthenticated,
         },
         {
             path: "/:pathMatch(.*)*",
             name: "not-found",
             component: NotFound,
-        }
+        },
+        {
+            path: "/web/:id",
+            name: "web",
+            component: WebSockets,
+        },
     ],
 });
 
