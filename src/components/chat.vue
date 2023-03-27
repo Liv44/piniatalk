7be62f4d325866ab1f00c41809e5d9b048013bc1
Luke859 <71589052+Luke859@users.noreply.github.com>
2023-03-27 11:52:03 +0200

    add message

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -18,182 +18,182 @@ const image = ref("");
 const inputFile = ref(null);
 
 const onChange = (event: Event) => {
-    const input = event.target as HTMLInputElement;
-    const file = input.files![0];
-    image.value = URL.createObjectURL(file);
+  const input = event.target as HTMLInputElement;
+  const file = input.files![0];
+  image.value = URL.createObjectURL(file);
 };
 
 const add = async () => {
-    const image = (inputFile.value as unknown as HTMLInputElement).value;
-    if (message.value && !image) {
-        addMessage(id.toString(), { type: "text", data: message.value });
-        message.value = "";
-    } else if (image && !message.value) {
-        addMessage(id.toString(), { type: "file", data: image });
-        (inputFile.value as unknown as HTMLInputElement).value = "";
-    } else {
-        alert("Entrez soit du texte, soit une image.");
-        console.log("Entrez soit du texte, soit une image.");
-        (inputFile.value as unknown as HTMLInputElement).value = "";
-        message.value = "";
-    }
+  const image = (inputFile.value as unknown as HTMLInputElement).value;
+  if (message.value && !image) {
+    addMessage(id.toString(), { type: "text", data: message.value });
+    message.value = "";
+  } else if (image && !message.value) {
+    addMessage(id.toString(), { type: "file", data: image });
+    (inputFile.value as unknown as HTMLInputElement).value = "";
+  } else {
+    alert("Entrez soit du texte, soit une image.");
+    console.log("Entrez soit du texte, soit une image.");
+    (inputFile.value as unknown as HTMLInputElement).value = "";
+    message.value = "";
+  }
 };
 </script>
 
 <template>
-    <div class="block_chat">
-        <div class="message">
-            <div
-                class="message-list"
-                v-for="(messages, index) of messageList"
-                :key="index"
-            >
-                <div class="message-content">
-                    <div class="message-header">
-                        <div class="user-img">
-                            <img src="../assets/luke3.JPG" />
-                        </div>
-                        <!-- <h4 class="username">{{ messages.author }}</h4> -->
-                        <!-- <span class="timestamp">{{
+  <div class="block_chat">
+    <div class="message">
+      <div
+        class="message-list"
+        v-for="(messages, index) of messageList"
+        :key="index"
+      >
+        <div class="message-content">
+          <div class="message-header">
+            <div class="user-img">
+              <img src="../assets/luke3.JPG" />
+            </div>
+            <!-- <h4 class="username">{{ messages.author }}</h4> -->
+            <!-- <span class="timestamp">{{
               new Date(messages.timestamp * 1000)
             }}</span> -->
-                    </div>
-                    <p class="chat-message" v-if="messages.text">
-                        {{ messages.text }}
-                    </p>
-                    <img
-                        v-if="messages.image"
-                        :src="messages.image"
-                        style="height: auto; width: auto; max-height: 300px"
-                    />
-                    <!-- {{ messages.channel_id }} -->
-                </div>
-            </div>
+          </div>
+          <!-- <p class="chat-message" v-if="messages.text">
+            {{ messages.text }}
+          </p>
+          <img
+            v-if="messages.image"
+            :src="messages.image"
+            style="height: auto; width: auto; max-height: 300px"
+          /> -->
+          <!-- {{ messages.channel_id }} -->
         </div>
-        <div class="chat-input">
-            <label for="file-input">
-                <i
-                    class="bi bi-plus-circle"
-                    style="font-size: 1.5rem; color: white; margin-right: 10px"
-                >
-                    <input
-                        type="file"
-                        ref="inputFile"
-                        id="file-input"
-                        accept="image/*"
-                        style="display: none"
-                        @change="onChange"
-                    />
-                </i>
-            </label>
-            <input
-                class="input"
-                type="text"
-                v-model="message"
-                required
-                placeholder="Message...."
-            />
-            <button @click="add">Send</button>
-        </div>
+      </div>
     </div>
+    <div class="chat-input">
+      <label for="file-input">
+        <i
+          class="bi bi-plus-circle"
+          style="font-size: 1.5rem; color: white; margin-right: 10px"
+        >
+          <input
+            type="file"
+            ref="inputFile"
+            id="file-input"
+            accept="image/*"
+            style="display: none"
+            @change="onChange"
+          />
+        </i>
+      </label>
+      <input
+        class="input"
+        type="text"
+        v-model="message"
+        required
+        placeholder="Message...."
+      />
+      <button @click="add">Send</button>
+    </div>
+  </div>
 </template>
 
 <style>
 .block_chat {
-    position: fixed;
-    background-color: #c9c9c9;
-    top: 100px;
-    margin: auto;
-    height: 750px;
-    overflow: auto;
-    border-radius: 10px;
-    padding: 10px;
+  position: fixed;
+  background-color: #c9c9c9;
+  top: 100px;
+  margin: auto;
+  height: 750px;
+  overflow: auto;
+  border-radius: 10px;
+  padding: 10px;
 }
 
 /* User image */
 .user-img {
-    margin-right: 10px;
+  margin-right: 10px;
 }
 
 .user-img img {
-    border-radius: 50%;
-    height: 50px;
-    width: 50px;
+  border-radius: 50%;
+  height: 50px;
+  width: 50px;
 }
 
 /* Message list*/
 .message {
-    overflow-y: scroll;
+  overflow-y: scroll;
 }
 
 .message-list {
-    display: flex;
-    margin-bottom: 10px;
-    padding: 10px;
+  display: flex;
+  margin-bottom: 10px;
+  padding: 10px;
 }
 
 /* Message Content */
 .message-content {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 /* Message Header */
 .message-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 5px;
 }
 
 .message-header .username {
-    color: #353030;
-    font-weight: bold;
-    margin-right: 10px;
+  color: #353030;
+  font-weight: bold;
+  margin-right: 10px;
 }
 
 .message-header .timestamp {
-    color: #8a8a8a;
+  color: #8a8a8a;
 }
 
 /* Message Text */
 .chat-message {
-    color: black;
-    margin: 0;
+  color: black;
+  margin: 0;
 }
 
 /* Chat Input */
 .chat-input {
-    position: fixed;
-    bottom: 0;
-    left: 300px;
-    right: 0;
-    width: auto;
-    align-items: center;
-    padding: 10px;
-    background-color: #353030;
-    border-radius: 10px;
+  position: fixed;
+  bottom: 0;
+  left: 300px;
+  right: 0;
+  width: auto;
+  align-items: center;
+  padding: 10px;
+  background-color: #353030;
+  border-radius: 10px;
 }
 
 .chat-input input[type="text"] {
-    border: none;
-    border-radius: 10px;
-    padding: 10px;
-    margin-right: 10px;
-    width: 90%;
+  border: none;
+  border-radius: 10px;
+  padding: 10px;
+  margin-right: 10px;
+  width: 90%;
 }
 
 .chat-input button {
-    background-color: #ffe46b;
-    color: black;
-    border: none;
-    border-radius: 10px;
-    padding: 8px;
+  background-color: #ffe46b;
+  color: black;
+  border: none;
+  border-radius: 10px;
+  padding: 8px;
 }
 
 .chat-input input[type="file"] {
-    border: none;
-    width: 16px;
-    height: 16px;
+  border: none;
+  width: 16px;
+  height: 16px;
 }
 </style>
