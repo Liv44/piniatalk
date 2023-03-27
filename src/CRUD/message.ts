a74214e77bf5e4b5ede4e82336caa30e8b45819f
Elouan <73824665+ByMSRT@users.noreply.github.com>
2023-03-27 09:54:16 +0200

    Fix chat component

--- src/CRUD/message.ts
+++ src/CRUD/message.ts
@@ -1,107 +1,111 @@
 import axios from "axios";
 import { ref, reactive } from "vue";
-import { defineStore } from "pinia"
+import { defineStore } from "pinia";
 
 const baseURL = "https://edu.tardigrade.land/msg";
 
 export const postLogin = (endPoint: string) => {
     try {
-        return axios
-            .post(
-                baseURL + endPoint,
-                {
-                    username: "e.dumont",
-                    password: "5fd6cdd6cced416f",
+        return axios.post(
+            baseURL + endPoint,
+            {
+                username: "e.dumont",
+                password: "5fd6cdd6cced416f",
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
                 },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            )
-
+            }
+        );
     } catch (error) {
         console.log(error);
     }
 };
 
 type MessageContent = {
-    type : string;
-    data : string;
-}
+    type: string;
+    data: string;
+};
 
 interface Message {
-    content?: MessageContent
-  }
+    content?: MessageContent;
+}
 
 export const useMessageStore = defineStore("messageList", {
-    state : () => ({
+    state: () => ({
         messageList: [] as Message[],
         id: 0,
-        error: null
+        error: null,
     }),
     actions: {
-
         /* POST messages*/
-        async addMessage(channel_id: number, content?: MessageContent){
-
+        async addMessage(channel_id: string, content?: MessageContent) {
             try {
-                if ( content?.type === 'text') {
-                const response = await axios.post(`${baseURL}/protected/channel/${channel_id}/message`,
-                    {
-                        "Text": content?.data,
-                    },
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+                if (content?.type === "text") {
+                    const response = await axios.post(
+                        `${baseURL}/protected/channel/${channel_id}/message`,
+                        {
+                            Text: content?.data,
                         },
-                    }
-                );
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${sessionStorage.getItem(
+                                    "token"
+                                )}`,
+                            },
+                        }
+                    );
                     console.log(response.data);
                     return response.data;
-
-                } else if( content?.type === 'file')  {
-                    const response = await axios.post(`${baseURL}/protected/channel/${channel_id}/message`,
-                    {
-                        "Image": content?.data,
-                    },
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+                } else if (content?.type === "file") {
+                    const response = await axios.post(
+                        `${baseURL}/protected/channel/${channel_id}/message`,
+                        {
+                            Image: content?.data,
                         },
-                    }
-                );
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${sessionStorage.getItem(
+                                    "token"
+                                )}`,
+                            },
+                        }
+                    );
                     console.log(response.data);
                     return response.data;
-                }  else {
-                    console.log('Entrez soit du texte, soit une image.');
-                }     
-            } catch(error) {
-                console.log("Error" + error)
+                } else {
+                    console.log("Entrez soit du texte, soit une image.");
+                }
+            } catch (error) {
+                console.log("Error" + error);
             }
         },
 
         /* Get all message of channel */
-        async getMessages(channel_id: string, numberMessage: number ){
+        async getMessages(channel_id: string, numberMessage: number) {
             try {
-                return axios.get(baseURL +`/protected/channel/${channel_id}/messages/${numberMessage}`,
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: `Bearer ${sessionStorage.getItem(
-                                "token"
-                            )}`,
-                        },
-                    }
-                ).then((response) => {
-                    return response.data;
-                });
+                return axios
+                    .get(
+                        baseURL +
+                            `/protected/channel/${channel_id}/messages/${numberMessage}`,
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${sessionStorage.getItem(
+                                    "token"
+                                )}`,
+                            },
+                        }
+                    )
+                    .then((response) => {
+                        return response.data;
+                    });
             } catch (error) {
                 console.log(error);
             }
-        }
-    }  
-})
-
+        },
+    },
+});
--- src/components/chat.vue
+++ src/components/chat.vue
@@ -2,7 +2,10 @@
 import { ref, reactive } from "vue";
 import { useMessageStore } from "../CRUD/message";
 import { storeToRefs } from "pinia";
+import { useRoute } from "vue-router";
 
+const route = useRoute();
+const id = route.params.id;
 const store = useMessageStore();
 const { messageList } = storeToRefs(store);
 const { addMessage } = store;
@@ -10,187 +13,187 @@ const { addMessage } = store;
 const message = ref("");
 const timestamp = ref(new Date());
 const author = ref("Luke");
-const channel_id = ref(96);
+
 const image = ref("");
 const inputFile = ref(null);
 
 const onChange = (event: Event) => {
-  const input = event.target as HTMLInputElement;
-  const file = input.files![0];
-  image.value = URL.createObjectURL(file);
+    const input = event.target as HTMLInputElement;
+    const file = input.files![0];
+    image.value = URL.createObjectURL(file);
 };
 
 const add = async () => {
-  const image = (inputFile.value as unknown as HTMLInputElement).value;
-  if (message.value && !image) {
-    addMessage(channel_id.value, { type: "text", data: message.value });
-    message.value = "";
-  } else if (image && !message.value) {
-    addMessage(channel_id.value, { type: "file", data: image });
-    (inputFile.value as unknown as HTMLInputElement).value = "";
-  } else {
-    alert("Entrez soit du texte, soit une image.");
-    console.log("Entrez soit du texte, soit une image.");
-    (inputFile.value as unknown as HTMLInputElement).value = "";
-    message.value = "";
-  }
+    const image = (inputFile.value as unknown as HTMLInputElement).value;
+    if (message.value && !image) {
+        addMessage(id.toString(), { type: "text", data: message.value });
+        message.value = "";
+    } else if (image && !message.value) {
+        addMessage(id.toString(), { type: "file", data: image });
+        (inputFile.value as unknown as HTMLInputElement).value = "";
+    } else {
+        alert("Entrez soit du texte, soit une image.");
+        console.log("Entrez soit du texte, soit une image.");
+        (inputFile.value as unknown as HTMLInputElement).value = "";
+        message.value = "";
+    }
 };
 </script>
 
 <template>
-  <div class="block_chat">
-    <div class="message">
-      <div
-        class="message-list"
-        v-for="(messages, index) of messageList"
-        :key="index"
-      >
-        <div class="message-content">
-          <div class="message-header">
-            <div class="user-img">
-              <img src="../assets/luke3.JPG" />
-            </div>
-            <!-- <h4 class="username">{{ messages.author }}</h4> -->
-            <!-- <span class="timestamp">{{
+    <div class="block_chat">
+        <div class="message">
+            <div
+                class="message-list"
+                v-for="(messages, index) of messageList"
+                :key="index"
+            >
+                <div class="message-content">
+                    <div class="message-header">
+                        <div class="user-img">
+                            <img src="../assets/luke3.JPG" />
+                        </div>
+                        <!-- <h4 class="username">{{ messages.author }}</h4> -->
+                        <!-- <span class="timestamp">{{
               new Date(messages.timestamp * 1000)
             }}</span> -->
-          </div>
-          <p class="chat-message" v-if="messages.text">
-            {{ messages.text }}
-          </p>
-          <img
-            v-if="messages.image"
-            :src="messages.image"
-            style="height: auto; width: auto; max-height: 300px"
-          />
-          <!-- {{ messages.channel_id }} -->
+                    </div>
+                    <p class="chat-message" v-if="messages.text">
+                        {{ messages.text }}
+                    </p>
+                    <img
+                        v-if="messages.image"
+                        :src="messages.image"
+                        style="height: auto; width: auto; max-height: 300px"
+                    />
+                    <!-- {{ messages.channel_id }} -->
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="chat-input">
+            <label for="file-input">
+                <i
+                    class="bi bi-plus-circle"
+                    style="font-size: 1.5rem; color: white; margin-right: 10px"
+                >
+                    <input
+                        type="file"
+                        ref="inputFile"
+                        id="file-input"
+                        accept="image/*"
+                        style="display: none"
+                        @change="onChange"
+                    />
+                </i>
+            </label>
+            <input
+                class="input"
+                type="text"
+                v-model="message"
+                required
+                placeholder="Message...."
+            />
+            <button @click="add">Send</button>
+        </div>
     </div>
-    <div class="chat-input">
-      <label for="file-input">
-        <i
-          class="bi bi-plus-circle"
-          style="font-size: 1.5rem; color: white; margin-right: 10px"
-        >
-          <input
-            type="file"
-            ref="inputFile"
-            id="file-input"
-            accept="image/*"
-            style="display: none"
-            @change="onChange"
-          />
-        </i>
-      </label>
-      <input
-        class="input"
-        type="text"
-        v-model="message"
-        required
-        placeholder="Message...."
-      />
-      <button @click="add">Send</button>
-    </div>
-  </div>
 </template>
 
 <style>
 .block_chat {
-  position: fixed;
-  background-color: #c9c9c9;
-  top: 100px;
-  margin: auto;
-  height: 750px;
-  overflow: auto;
-  border-radius: 10px;
-  padding: 10px;
+    position: fixed;
+    background-color: #c9c9c9;
+    top: 100px;
+    margin: auto;
+    height: 750px;
+    overflow: auto;
+    border-radius: 10px;
+    padding: 10px;
 }
 
 /* User image */
 .user-img {
-  margin-right: 10px;
+    margin-right: 10px;
 }
 
 .user-img img {
-  border-radius: 50%;
-  height: 50px;
-  width: 50px;
+    border-radius: 50%;
+    height: 50px;
+    width: 50px;
 }
 
 /* Message list*/
 .message {
-  overflow-y: scroll;
+    overflow-y: scroll;
 }
 
 .message-list {
-  display: flex;
-  margin-bottom: 10px;
-  padding: 10px;
+    display: flex;
+    margin-bottom: 10px;
+    padding: 10px;
 }
 
 /* Message Content */
 .message-content {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 }
 
 /* Message Header */
 .message-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 5px;
 }
 
 .message-header .username {
-  color: #353030;
-  font-weight: bold;
-  margin-right: 10px;
+    color: #353030;
+    font-weight: bold;
+    margin-right: 10px;
 }
 
 .message-header .timestamp {
-  color: #8a8a8a;
+    color: #8a8a8a;
 }
 
 /* Message Text */
 .chat-message {
-  color: black;
-  margin: 0;
+    color: black;
+    margin: 0;
 }
 
 /* Chat Input */
 .chat-input {
-  position: fixed;
-  bottom: 0;
-  left: 300px;
-  right: 0;
-  width: auto;
-  align-items: center;
-  padding: 10px;
-  background-color: #353030;
-  border-radius: 10px;
+    position: fixed;
+    bottom: 0;
+    left: 300px;
+    right: 0;
+    width: auto;
+    align-items: center;
+    padding: 10px;
+    background-color: #353030;
+    border-radius: 10px;
 }
 
 .chat-input input[type="text"] {
-  border: none;
-  border-radius: 10px;
-  padding: 10px;
-  margin-right: 10px;
-  width: 90%;
+    border: none;
+    border-radius: 10px;
+    padding: 10px;
+    margin-right: 10px;
+    width: 90%;
 }
 
 .chat-input button {
-  background-color: #ffe46b;
-  color: black;
-  border: none;
-  border-radius: 10px;
-  padding: 8px;
+    background-color: #ffe46b;
+    color: black;
+    border: none;
+    border-radius: 10px;
+    padding: 8px;
 }
 
 .chat-input input[type="file"] {
-  border: none;
-  width: 16px;
-  height: 16px;
+    border: none;
+    width: 16px;
+    height: 16px;
 }
 </style>
