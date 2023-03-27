213280197e3b692209063f5812a42050ceb89c18
Olivia Moreau <moreauolivia49@gmail.com>
2023-03-27 15:42:52 +0200

    add ChatComponent to HomePage

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { createApp } from "vue";
 import { createPinia } from 'pinia'
 import "./style.css";
 import App from "./App.vue";
-import { createPinia } from "pinia";
 import router from "./router";
 
 const pinia = createPinia();
 const app = createApp(App);
-const pinia = createPinia();
 
 app.use(router);
 app.use(pinia);
